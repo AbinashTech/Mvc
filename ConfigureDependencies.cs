d754752ecd29c2abf1cf77c5260a99931c2891c4
Abinash-8892 <87180988+Abinash-8892@users.noreply.github.com>
2022-09-20 14:41:22 +0530

    Add files via upload

---
+++ ConfigureDependencies.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WeighingAutomation.LCS.DAL;
+using WeighingAutomation.LCS.DAL.Entities;
+using WeighingAutomation.LCS.Repositories.Implementations;
+using WeighingAutomation.LCS.Repositories.Interfaces;
+using WeighingAutomation.LCS.Services.Implementations;
+using WeighingAutomation.LCS.Services.Interfaces;
+
+namespace WeighingAutomation.LCS.Services
+{
+    public class ConfigureDependencies
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            //database
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddScoped<DbContext, AppDbContext>();
+
+            //repositories
+            services.AddScoped<IRepository<User>, Repository<User>>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            //services
+            services.AddScoped<IAuthService, AuthService>();
+        }
+    }
+}
